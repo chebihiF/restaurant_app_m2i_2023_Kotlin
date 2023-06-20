e1215e160807f9c3aa83c0dbc3e955a91a037035
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-06-20 08:21:03 +0100

    Initial commit

---
+++ app/src/main/java/org/m2i/restaurant_app/RestaurantsScreen.kt
@@ -0,0 +1,17 @@
+package org.m2i.restaurant_app
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RestaurantsScreen(){
+    LazyColumn(contentPadding = PaddingValues(
+        vertical = 8.dp,
+        horizontal = 8.dp
+    )){
+     
+    }
+}
